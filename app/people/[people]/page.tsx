0f447c08e1a8ffd6c79fb5c1194e144b46f4afd6
GLancer14 <181259602+GLancer14@users.noreply.github.com>
2026-07-01 17:07:52 +0400

    Start to work with people page

--- app/people/[people]/page.tsx
+++ app/people/[people]/page.tsx
@@ -7,6 +7,8 @@ import Header from "../../(ui)/Header/Header";
 import Footer from "../../(ui)/Footer/Footer";
 import { prisma } from "../../lib/prisma";
 import SongPage from "../../(ui)/SongPage/SongPage";
+import Image from "next/image";
+import clsx from "clsx";
 
 export default async function Page({
   params,
@@ -22,6 +24,30 @@ export default async function Page({
     },
   });
 
+  const songs = await prisma.songs_people.findMany({
+    where: {
+      id: +people,
+    },
+    orderBy: {
+      songs: {
+        song_id: "asc"
+      }
+    },
+    take: 8,
+    include: {
+      songs: true,
+    }
+  });
+
+  const albums = await prisma.songs_albums.findMany({
+    where: {
+      id: +people,
+    },
+    include: {
+      albums: true,
+    }
+  });
+
   const country = await prisma.countries.findFirst({
     where: {
       country_id: peopleData?.country_id ? peopleData?.country_id : 0,
@@ -33,26 +59,82 @@ export default async function Page({
   return (
     <>
       <Header user={userData} />
-      <div className="flex flex-col flex-1">
-        {people && 
-          (<div>
-            <div>
-              <img
-                src={`/backgrounds/people/${peopleData?.image}`}
-                width={100}
-                height={100}
-                alt="Обложка"
-              />
+      {peopleData && 
+        <div className="flex flex-col flex-1 items-center">
+          <header
+            className={`flex items-center relative h-82.5 w-full justify-center bg-no-repeat bg-cover bg-center`}
+            style={{
+              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1) 0%, rgba(0, 0, 0, 0.5) 100%), url(/backgrounds/people/${peopleData.image})`,
+            }}
+          >
+            <div className="relative colum flex flex-row max-w-344 w-344 py-4 px-10.5">
+              {peopleData && peopleData.image &&
+                <Image
+                  className="relative top-12 self-start mr-11 border-4 border-white rounded-[50%] object-cover object-center w-85 h-85"
+                  src={peopleData.image ? `/backgrounds/people/${peopleData.image}` : "/noimage2.svg"}
+                  alt={"обложка"}
+                  width={340}
+                  height={340}
+                />}
+                <div className="relative top-4 flex flex-col flex-1 justify-end-safe text-white">
+                  <div className="text-[90px] mb-2">{peopleData?.name}</div>
+                </div>
             </div>
-            <div>{peopleData?.name}</div>
-            <div>{peopleData?.nickname}</div>
-            <div>{peopleData?.firstname}</div>
-            <div>{peopleData?.surname}</div>
-            <div>{peopleData?.description}</div>
-            <div>{country?.country}</div>
-          </div>)
-        }
-      </div>
+            <div
+              className={clsx(`
+                absolute
+                w-full
+                h-full
+                top-0
+                left-0
+                bg-cover
+                bg-no-repeat
+                bg-center
+                z-[-1]
+              `)}
+            ></div>
+          </header>
+          <div className="relative top-16 flex flex-row gap-17 w-300 max-w-300">
+            <div className="w-130 max-w-130">
+              <h3 className="text-center text-[40px]">About</h3>
+              {peopleData.description}
+            </div>
+            <div className="w-263 max-w-263">
+              <div>
+                <h3 className="text-[22.5px] font-medium">Songs</h3>
+                <div className="flex flex-row flex-wrap gap-4 border-gray-300 border-2 p-7">
+                  {songs.map(song => {
+                    return (
+                      <a
+                        className="flex flex-row flex-wrap w-[calc(100%/2-16px)]"
+                        href={`/songs/${song.song_id}`}
+                      >
+                        <Image
+                          src={song.songs.image ? `/backgrounds/songs/${song.songs.image}` : "/noimage2.svg"}
+                          alt={"обложка песни"}
+                          width={90}
+                          height={90}
+                        />
+                        <div className="my-2 ml-4">
+                          <div>{song.songs.name}</div>
+                          <div>{peopleData.name}</div>
+                        </div>
+                      </a>
+                    )
+                  })}
+                  <button
+                    className="w-full h-10 border border-black rounded-[20px] text-[18px] cursor-pointer"
+                    type="button"
+                  >
+                    All songs by {peopleData.name}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      } 
+      
       <Footer />
     </>
   )
